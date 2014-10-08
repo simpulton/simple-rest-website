b6d509b3cf26f7db589a1466a592ea27ff8fba45
Lukas Ruebbelke <simpul@gmail.com>
2014-10-08 11:55:22 -0700

    Small update in naming convention

--- public/app/app.js
+++ public/app/app.js
@@ -1,100 +1,100 @@
 angular.module('SimpleRESTWebsite', [])
-  .constant('ENDPOINT_URI', 'http://localhost:3000/api/')
-  .controller('MainCtrl', function (ItemsModel) {
-    var main = this;
+    .constant('ENDPOINT_URI', 'http://localhost:3000/api/')
+    .controller('MainCtrl', function (ItemsModel) {
+        var main = this;
 
-    function getItems() {
-      ItemsModel.all()
-        .then(function (result) {
-          main.items = result.data;
-        });
-    }
+        function getItems() {
+            ItemsModel.all()
+                .then(function (result) {
+                    main.items = result.data;
+                });
+        }
 
-    function createItem(item) {
-      ItemsModel.create(item)
-        .then(function (result) {
-          resetCreateForm();
-          getItems();
-        });
-    }
+        function createItem(item) {
+            ItemsModel.create(item)
+                .then(function (result) {
+                    initCreateForm();
+                    getItems();
+                });
+        }
 
-    function updateItem(item) {
-      ItemsModel.update(item.id, item)
-        .then(function (result) {
-          cancelEditing();
-          getItems();
-        });
-    }
+        function updateItem(item) {
+            ItemsModel.update(item.id, item)
+                .then(function (result) {
+                    cancelEditing();
+                    getItems();
+                });
+        }
 
-    function deleteItem(itemId) {
-      ItemsModel.destroy(itemId)
-        .then(function (result) {
-          cancelEditing();
-          getItems();
-        });
-    }
+        function deleteItem(itemId) {
+            ItemsModel.destroy(itemId)
+                .then(function (result) {
+                    cancelEditing();
+                    getItems();
+                });
+        }
 
-    function resetCreateForm() {
-      main.newItem = { name: '', description: '' };
-    }
+        function initCreateForm() {
+            main.newItem = { name: '', description: '' };
+        }
 
-    function setEditedItem(item) {
-      main.editedItem = angular.copy(item);
-      main.isEditing = true;
-    }
+        function setEditedItem(item) {
+            main.editedItem = angular.copy(item);
+            main.isEditing = true;
+        }
 
-    function isCurrentItem(itemId) {
-      return main.editedItem !== null && main.editedItem.id === itemId;
-    }
+        function isCurrentItem(itemId) {
+            return main.editedItem !== null && main.editedItem.id === itemId;
+        }
 
-    function cancelEditing() {
-      main.editedItem = null;
-      main.isEditing = false;
-    }
+        function cancelEditing() {
+            main.editedItem = null;
+            main.isEditing = false;
+        }
 
-    main.items = [];
-    main.editedItem = null;
-    main.isEditing = false;
-    main.newItem = { name: '', description: '' };
-    main.getItems = getItems;
-    main.createItem = createItem;
-    main.updateItem = updateItem;
-    main.deleteItem = deleteItem;
-    main.setEditedItem = setEditedItem;
-    main.isCurrentItem = isCurrentItem;
-    main.cancelEditing = cancelEditing;
+        main.items = [];
+        main.editedItem = null;
+        main.isEditing = false;
+        main.getItems = getItems;
+        main.createItem = createItem;
+        main.updateItem = updateItem;
+        main.deleteItem = deleteItem;
+        main.setEditedItem = setEditedItem;
+        main.isCurrentItem = isCurrentItem;
+        main.cancelEditing = cancelEditing;
 
-    getItems();
-  })
-  .service('ItemsModel', function ($http, ENDPOINT_URI) {
-    var service = this,
-    path = 'items/';
+        initCreateForm();
+        getItems();
+    })
+    .service('ItemsModel', function ($http, ENDPOINT_URI) {
+        var service = this,
+            path = 'items/';
 
-    function getUrl() {
-      return ENDPOINT_URI + path;
-    }
+        function getUrl() {
+            return ENDPOINT_URI + path;
+        }
 
-    function getUrlForId(itemId) {
-      return getUrl(path) + itemId;
-    }
+        function getUrlForId(itemId) {
+            return getUrl(path) + itemId;
+        }
 
-    service.all = function () {
-      return $http.get(getUrl());
-    };
+        service.all = function () {
+            return $http.get(getUrl());
+        };
 
-    service.fetch = function (itemId) {
-      return $http.get(getUrlForId(itemId));
-    };
+        service.fetch = function (itemId) {
+            return $http.get(getUrlForId(itemId));
+        };
 
-    service.create = function (item) {
-      return $http.post(getUrl(), item);
-    };
+        service.create = function (item) {
+            return $http.post(getUrl(), item);
+        };
 
-    service.update = function (itemId, item) {
-      return $http.put(getUrlForId(itemId), item);
-    };
+        service.update = function (itemId, item) {
+            return $http.put(getUrlForId(itemId), item);
+        };
 
-    service.destroy = function (itemId) {
-      return $http.delete(getUrlForId(itemId));
-    };
-  });
+        service.destroy = function (itemId) {
+            return $http.delete(getUrlForId(itemId));
+        };
+    });

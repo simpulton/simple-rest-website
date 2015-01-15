0cccf12c0881f7ea311d888d6208532d3593ffe5
Lukas Ruebbelke <simpul@gmail.com>
2015-01-15 15:11:07 -0700

    Simple auth

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,33 @@
 angular.module('SimpleRESTWebsite', [])
     .constant('ENDPOINT_URI', 'http://localhost:1337/api/')
-    .controller('MainCtrl', function (ItemsModel) {
+    .config(function($httpProvider) {
+        $httpProvider.interceptors.push('AccessTokenInterceptor');
+    })
+    .controller('MainCtrl', function (LoginService, UserService, ItemsModel) {
         var main = this;
+
+        function submit(user) {
+            main.newUser ? main.register(user) : main.login(user);
+        }
+
+        function login(user) {
+            LoginService.login(user)
+                .then(function(response) {
+                    user.access_token = response.data.id;
+                    main.currentUser = user;
+                    UserService.setCurrentUser(user);
+                    getItems();
+                }, function(error) {
+                    alert("We couldn't find that username/password combination :(");
+                });
+        }
+
+        function register(user) {
+            LoginService.register(user)
+                .then(function(response) {
+                    login(user);
+                });
+        }
 
         function getItems() {
             ItemsModel.all()
@@ -52,9 +78,14 @@ angular.module('SimpleRESTWebsite', [])
             main.isEditing = false;
         }
 
+        main.newUser = false;
+        main.currentUser = UserService.getCurrentUser();
         main.items = [];
         main.editedItem = null;
         main.isEditing = false;
+        main.login = login;
+        main.register = register;
+        main.submit = submit;
         main.getItems = getItems;
         main.createItem = createItem;
         main.updateItem = updateItem;
@@ -97,4 +128,54 @@ angular.module('SimpleRESTWebsite', [])
         service.destroy = function (itemId) {
             return $http.delete(getUrlForId(itemId));
         };
+    })
+    .service('LoginService', function($http, ENDPOINT_URI) {
+        var service = this,
+            path = 'Users/';
+
+        function getUrl() {
+            return ENDPOINT_URI + path;
+        }
+
+        function getLogUrl(action) {
+            return getUrl() + action;
+        }
+
+        service.login = function(credentials) {
+            return $http.post(getLogUrl('login'), credentials);
+        };
+
+        service.logout = function() {
+            return $http.post(getLogUrl('logout'));
+        };
+
+        service.register = function(user) {
+            return $http.post(getUrl(), user);
+        };
+    })
+    .service('UserService', function() {
+        var service = this,
+            currentUser = null;
+
+        service.setCurrentUser = function(user) {
+            currentUser = user;
+        };
+
+        service.getCurrentUser = function() {
+            return currentUser;
+        };
+    })
+    .factory('AccessTokenInterceptor', function(UserService) {
+       return {
+           request: function(config) {
+               var currentUser = UserService.getCurrentUser(),
+                   access_token = currentUser ? currentUser.access_token : null;
+
+               if (access_token) {
+                   config.headers.authorization = access_token;
+               }
+               return config;
+           }
+       }
     });
+;

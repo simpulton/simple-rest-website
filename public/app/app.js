2fa0c0b575c249a9a1517808fc46e932f3c32e72
Lukas Ruebbelke <simpul@gmail.com>
2015-01-20 08:39:11 -0700

    Remove node script and reorganize app code

--- public/app/app.js
+++ public/app/app.js
@@ -3,6 +3,92 @@ angular.module('SimpleRESTWebsite', ['angular-storage'])
     .config(function($httpProvider) {
         $httpProvider.interceptors.push('AccessTokenInterceptor');
     })
+    .service('AccessTokenInterceptor', function(UserService) {
+        var service = this;
+
+        service.request = function(config) {
+            var currentUser = UserService.getCurrentUser(),
+                access_token = currentUser ? currentUser.access_token : null;
+
+            if (access_token) {
+                config.headers.authorization = access_token;
+            }
+            return config;
+        }
+    })
+    .service('UserService', function(store) {
+        var service = this,
+            currentUser = null;
+
+        service.setCurrentUser = function(user) {
+            currentUser = user;
+            store.set('user', user);
+            return currentUser;
+        };
+
+        service.getCurrentUser = function() {
+            if (!currentUser) {
+                currentUser = store.get('user');
+            }
+            return currentUser;
+        };
+    })
+    .service('LoginService', function($http, ENDPOINT_URI) {
+        var service = this,
+            path = 'Users/';
+
+        function getUrl() {
+            return ENDPOINT_URI + path;
+        }
+
+        function getLogUrl(action) {
+            return getUrl() + action;
+        }
+
+        service.login = function(credentials) {
+            return $http.post(getLogUrl('login'), credentials);
+        };
+
+        service.logout = function() {
+            return $http.post(getLogUrl('logout'));
+        };
+
+        service.register = function(user) {
+            return $http.post(getUrl(), user);
+        };
+    })
+    .service('ItemsModel', function ($http, ENDPOINT_URI) {
+        var service = this,
+            path = 'items/';
+
+        function getUrl() {
+            return ENDPOINT_URI + path;
+        }
+
+        function getUrlForId(itemId) {
+            return getUrl(path) + itemId;
+        }
+
+        service.all = function () {
+            return $http.get(getUrl());
+        };
+
+        service.fetch = function (itemId) {
+            return $http.get(getUrlForId(itemId));
+        };
+
+        service.create = function (item) {
+            return $http.post(getUrl(), item);
+        };
+
+        service.update = function (itemId, item) {
+            return $http.put(getUrlForId(itemId), item);
+        };
+
+        service.destroy = function (itemId) {
+            return $http.delete(getUrlForId(itemId));
+        };
+    })
     .controller('MainCtrl', function (LoginService, UserService, ItemsModel) {
         var main = this;
 
@@ -107,90 +193,4 @@ angular.module('SimpleRESTWebsite', ['angular-storage'])
         initCreateForm();
         getItems();
     })
-    .service('ItemsModel', function ($http, ENDPOINT_URI) {
-        var service = this,
-            path = 'items/';
-
-        function getUrl() {
-            return ENDPOINT_URI + path;
-        }
-
-        function getUrlForId(itemId) {
-            return getUrl(path) + itemId;
-        }
-
-        service.all = function () {
-            return $http.get(getUrl());
-        };
-
-        service.fetch = function (itemId) {
-            return $http.get(getUrlForId(itemId));
-        };
-
-        service.create = function (item) {
-            return $http.post(getUrl(), item);
-        };
-
-        service.update = function (itemId, item) {
-            return $http.put(getUrlForId(itemId), item);
-        };
-
-        service.destroy = function (itemId) {
-            return $http.delete(getUrlForId(itemId));
-        };
-    })
-    .service('LoginService', function($http, ENDPOINT_URI) {
-        var service = this,
-            path = 'Users/';
-
-        function getUrl() {
-            return ENDPOINT_URI + path;
-        }
-
-        function getLogUrl(action) {
-            return getUrl() + action;
-        }
-
-        service.login = function(credentials) {
-            return $http.post(getLogUrl('login'), credentials);
-        };
-
-        service.logout = function() {
-            return $http.post(getLogUrl('logout'));
-        };
-
-        service.register = function(user) {
-            return $http.post(getUrl(), user);
-        };
-    })
-    .service('UserService', function(store) {
-        var service = this,
-            currentUser = null;
-
-        service.setCurrentUser = function(user) {
-            currentUser = user;
-            store.set('user', user);
-            return currentUser;
-        };
-
-        service.getCurrentUser = function() {
-            if (!currentUser) {
-                currentUser = store.get('user');
-            }
-            return currentUser;
-        };
-    })
-    .service('AccessTokenInterceptor', function(UserService) {
-        var service = this;
-
-        service.request = function(config) {
-            var currentUser = UserService.getCurrentUser(),
-                access_token = currentUser ? currentUser.access_token : null;
-
-            if (access_token) {
-                config.headers.authorization = access_token;
-            }
-            return config;
-        }
-    });
 ;
--- site.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-    app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-var server = app.listen(1338, function() {
-  console.log('Listening on port %d', server.address().port);
-});
